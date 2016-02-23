e407f919320e633d0a3059392f699296cf35bfc8
austin4e <aforry2@ycp.edu>
2016-02-23 12:52:44 -0500

    initial cljs commit

---
+++ src/cljs/ascii_never_dies/core.cljs
@@ -0,0 +1,8 @@
+;; create main project namespace
+(ns ascii-never-dies.core)
+
+;; enable cljs to print to JS console or browser
+(enable-console-print!)
+
+;; print to the console
+(println "Hello, World!")
