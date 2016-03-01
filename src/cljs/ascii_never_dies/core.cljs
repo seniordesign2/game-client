fcfb5a93523cf57f100dbf94ab146ea47ae58a1e
austin4e <aforry2@ycp.edu>
2016-03-01 15:24:16 -0500

    changed directories, set up an initial index.cljs.hl for a game view, changed app.css so html uses monospaced font with no linespacing to print our ascii

--- src/cljs/ascii_never_dies/core.cljs
+++
@@ -1,8 +0,0 @@
-;; create main project namespace
-(ns ascii-never-dies.core)
-
-;; enable cljs to print to JS console or browser
-(enable-console-print!)
-
-;; print to the console
-(println "Hello, World!")
