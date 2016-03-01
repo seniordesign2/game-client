aa22faf37a2a6086e952dcb7530b9e35934fc54b
austin4e <aforry2@ycp.edu>
2016-03-01 18:08:07 -0500

    some basic skeleton code for implementing a func to init the game board, in tiles.cljs. Not yet implemented, but predefined test vectors still work.

--- src/ascii_never_dies/tiles.cljs
+++ src/ascii_never_dies/tiles.cljs
@@ -1,5 +1,8 @@
 (ns ascii-never-dies.tiles
-  (:require [clojure.string :as str]))
+  (:require [clojure.string]))
+
+;; empty hash-map for game board
+(def board {})
 
 ;; vectors to be updated with game info
 (def vRowA (vector "#" "#" "#" "#" "#" "#" "#" "#" "#" "#" "#"))
@@ -14,3 +17,18 @@
 (def strRowC (clojure.string/join vRowC))
 (def strRowD (clojure.string/join vRowD))
 (def strRowE (clojure.string/join vRowE))
+
+;; TODO: initialize game board with empty spaces (periods)
+;;       find way to pass as a string for index.cljs.hl
+
+(defn initBoard [v] 
+  (loop [i 0]
+    (when (< i 10)
+    (loop [j 0 vec v]
+      (when (< j 10)
+      (recur (inc j) (conj vec "."))))
+    (recur (inc i))))
+    v
+)
+
+(def strTest (clojure.string/join (initBoard [])))
