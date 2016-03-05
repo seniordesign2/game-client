47d5d949a26f50eac0769413da3d4dfd7358ae6a
decker <adecker5@ycp.edu>
2016-03-05 17:56:23 -0500

    Added new lines between lines of game board, though it's not working as expected. Also removed dependency on 'clojure.string'

--- src/ascii_never_dies/tiles.cljs
+++ src/ascii_never_dies/tiles.cljs
@@ -1,13 +1,12 @@
-(ns ascii-never-dies.tiles
-  (:require [clojure.string]))
+(ns ascii-never-dies.tiles)
 
 (enable-console-print!)
 
 ;; empty hash-map for game board
 (def board (atom []))
 (def boardStr (atom ""))
-(def height 100)
-(def width 100)
+(def height 10)
+(def width 20)
 
 ;; vectors to be updated with game info
 (def vRowA (vector "#" "#" "#" "#" "#" "#" "#" "#" "#" "#" "#"))
@@ -17,30 +16,28 @@
 (def vRowE (vector "#" "#" "#" "#" "#" "#" "#" "#" "#" "#" "#"))
 
 ;; string sequences of each vector to be printed by index.cljs.hl
-(def strRowA (clojure.string/join vRowA))
-(def strRowB (clojure.string/join vRowB))
-(def strRowC (clojure.string/join vRowC))
-(def strRowD (clojure.string/join vRowD))
-(def strRowE (clojure.string/join vRowE))
+(def strRowA (apply str vRowA))
+(def strRowB (apply str vRowB))
+(def strRowC (apply str vRowC))
+(def strRowD (apply str vRowD))
+(def strRowE (apply str vRowE))
 
 ;; initializes game board with empty spaces (periods)
 (defn initBoard [] 
   (dotimes [n (* width height)]
-    (swap! board conj ".")
-   )
-)
+    (swap! board conj ".")))
 
 ;; TODO: printBoard should create a gameboard string seperated by newlines based on width and height
-;(def printBoard
-;  (do
-;    (reset! boardStr "") ;;erase old board to avoid appending new string to old game board
-;    (dotimes [n height]
-;      (let [row (* n width)]
-;        (swap! boardStr conj (clojure.string/join (subvec (deref board) row (+ row width)))))
-;      )
-;    (deref boardStr)
-;  )
-;)
+(def printBoard
+  (do
+    (initBoard)
+    (reset! boardStr "") ;;erase old board to avoid appending new string to old game board
+    (dotimes [n height]
+      (let [row (* n width)]
+        (swap! boardStr str (apply str (subvec (deref board) row (+ row width))) "\n")))
+    (deref boardStr)))
 
 ;; passes a string made from the initBoard func
-(def strTest (do (initBoard) (clojure.string/join (deref board))))
+(def strTest (do
+                (initBoard)
+                (apply str (deref board))))
