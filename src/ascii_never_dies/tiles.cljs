f809fe7750c3f2d51d4b60e4a90abe767e183d77
decker <adecker5@ycp.edu>
2016-03-07 12:00:29 -0500

    Removed boardStr atom and made board printing loop based

--- src/ascii_never_dies/tiles.cljs
+++ src/ascii_never_dies/tiles.cljs
@@ -4,23 +4,8 @@
 
 ;; empty hash-map for game board
 (def board (atom []))
-(def boardStr (atom ""))
 (def height 10)
 (def width 20)
-
-;; vectors to be updated with game info
-(def vRowA (vector "#" "#" "#" "#" "#" "#" "#" "#" "#" "#" "#"))
-(def vRowB (vector "#" "." "." "." "." "." "." "." "B" "." "#"))
-(def vRowC (vector "#" "." "." "." "." "@" "." "." "." "." "#"))
-(def vRowD (vector "#" "." "B" "." "." "." "." "." "." "." "#"))
-(def vRowE (vector "#" "#" "#" "#" "#" "#" "#" "#" "#" "#" "#"))
-
-;; string sequences of each vector to be printed by index.cljs.hl
-(def strRowA (apply str vRowA))
-(def strRowB (apply str vRowB))
-(def strRowC (apply str vRowC))
-(def strRowD (apply str vRowD))
-(def strRowE (apply str vRowE))
 
 ;; initializes game board with empty spaces (periods)
 (defn initBoard [] 
@@ -31,11 +16,13 @@
 (def printBoard
   (do
     (initBoard)
-    (reset! boardStr "") ;;erase old board to avoid appending new string to old game board
-    (dotimes [n height]
-      (let [row (* n width)]
-        (swap! boardStr str (apply str (subvec (deref board) row (+ row width))) "\n")))
-    (deref boardStr)))
+    (loop [n 0
+           bStr ""]
+      (if (>= n height)
+        bStr
+        (let [row (* n width)]
+          (recur (inc n)
+                 (str bStr (apply str (subvec (deref board) row (+ row width))) "\n")))))))
 
 ;; passes a string made from the initBoard func
 (def strTest (do
