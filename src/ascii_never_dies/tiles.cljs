6ba63b0e472595817efa66799d0a6ceb2c1492d9
decker <adecker5@ycp.edu>
2016-03-15 13:35:56 -0400

    converted print-board to use new screen functions

--- src/ascii_never_dies/tiles.cljs
+++ src/ascii_never_dies/tiles.cljs
@@ -5,37 +5,9 @@
 (enable-console-print!)
 
 ;; empty hash-map for game board
-(def board (atom []))
 (def height 5)
 (def width 15)
-
-;; initializes game board with empty spaces (periods)
-(defn init-board [] 
-  (dotimes [n (* width height)]
-    (swap! board conj ".")))
-
-;; creates a gameboard string seperated by newlines based on width and height
-(defn print-board []
-  (do
-    (init-board)
-    ;; TODO: this will need removed when a main game loop initializes the player beforehand
-    (let [temp-board (assoc (deref board) (deref player/pos) "@")]
-      (loop [n 0
-             b-str ""]
-        (if (>= n height)
-          b-str
-          (let [row (* n width)]
-            (recur (inc n)
-                   (str b-str (apply str (subvec temp-board row (+ row width))) "\n"))))))))
-
-;; passes a string made from the initBoard func
-(def str-test (do
-                (init-board)
-                (apply str (deref board))))
-
-; testing .screen namespace
-(def scr (s/new-screen width height))
-(defn test-print [] (s/stringify (s/insert-string 5 3 "testing" (s/clear-screen "#" scr))))
+(def board (atom (s/new-screen width height)))
 
 ;; convert values from 1D board vec to 2D coord representation and vice versa
 ;; TODO: to be used later for movement methods and game logic and general map building
@@ -45,6 +17,26 @@
   (+ x (* y width)))
 
 (defn one-to-two-d [pos]
-  (let [x (mod pos width )
-        y (/ pos width)]
+  (let [x (int (mod pos width))
+        y (int (/ pos width))]
     [x y]))
+  
+;; initializes game board with empty spaces (periods)
+(defn init-board [] 
+  (dotimes [n (* width height)]
+    (swap! board conj ".")))
+
+;; creates a gameboard string seperated by newlines based on width and height
+(defn print-board []
+  (let [x 10
+        y 3]
+    (s/stringify (s/insert x y "@" (deref board)))))
+
+;; passes a string made from the initBoard func
+(def str-test (do
+                (init-board)
+                (apply str (deref board))))
+
+; testing .screen namespace
+(def scr (s/new-screen width height))
+(defn test-print [] (s/stringify (s/insert-string 5 3 "testing" (s/clear-screen "#" scr))))
