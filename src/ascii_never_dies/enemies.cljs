069674868a52d2c0867e2e4efe895e4e501284c7
decker <adecker5@ycp.edu>
2016-04-25 11:47:51 -0400

    Added enemies namespace

---
+++ src/ascii_never_dies/enemies.cljs
@@ -0,0 +1,19 @@
+(ns ascii-never-dies.enemies)
+
+(def Enemy {:glyph nil :type nil :cur-health nil :max-health nil})
+
+;; List of all enemies
+(def enemies [(assoc Enemy
+                     :glyph "r"
+                     :type "rat"
+                     :max-health 2)
+              (assoc Enemy
+                     :glyph "g"
+                     :type "goblin"
+                     :max-health 5)])
+
+(defn get-random-enemy
+  "Returns a random enemy type."
+  [tile]
+  (let [enemy (merge tile (rand-nth enemies))]
+    (assoc enemy :cur-health (:max-health enemy))))
