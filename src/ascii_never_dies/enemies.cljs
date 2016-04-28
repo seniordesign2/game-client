794915b071829c0eef75a04c5fbb0c1dc71bfa65
Daniel Muckerman <danielmuckerman@me.com>
2016-04-28 14:41:18 -0400

    Assign colors to enemies, start board as empty

--- src/ascii_never_dies/enemies.cljs
+++ src/ascii_never_dies/enemies.cljs
@@ -1,45 +1,53 @@
 (ns ascii-never-dies.enemies)
 
-(def Enemy {:glyph nil :type nil :cur-health nil :max-health nil :x nil :y nil})
+(def Enemy {:glyph nil :type nil :color nil :cur-health nil :max-health nil :x nil :y nil})
 
 ;; List of all enemies
 (def enemies [(assoc Enemy
                      :glyph "r"
                      :type "rat"
+                     :color "red"
                      :max-health 2)
               (assoc Enemy
                      :glyph "g"
                      :type "goblin"
+                     :color "blue"
                      :max-health 5)
 
               (assoc Enemy
                      :glyph "s"
                      :type "spoopy spider"
+                     :color "green"
                      :max-health 10)
 
               (assoc Enemy 
                      :glyph "d"
                      :type "demon"
+                     :color "pink"
                      :max-health 666)
 
               (assoc Enemy
                      :glyph "p"
                      :type "poltergeist"
+                     :color "white"
                      :max-health 1000)
 
               (assoc Enemy
                      :glyph "m"
                      :type "money bag"
+                     :color "brown"
                      :max-health 7777)
 
               (assoc Enemy
                      :glyph "n"
                      :type "Ninja Brian"
+                     :color "black"
                      :max-health 69)
 
               (assoc Enemy
                      :glyph "b"
                      :type "Mr. Bones"
+                     :color "chucknorris"
                      :max-health 1)])
 
 (defn get-random-enemy
